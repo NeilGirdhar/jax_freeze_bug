08561063c55fd0a7301f35c6f495d1aa329f9bce
Neil Girdhar <mistersheik@gmail.com>
2023-01-18 19:43:54 -0500

    .

--- jfb/one.py
+++ jfb/one.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
 from collections.abc import Callable, Iterable
-from dataclasses import KW_ONLY
-from dataclasses import dataclass as p_dataclass
 from functools import partial
 from itertools import chain
 from typing import Any, TypeVar
@@ -11,7 +9,7 @@ import haiku as hk
 import jax.numpy as jnp
 import numpy as np
 import tensorflow_datasets as tfds
-from efax import MultivariateUnitNormalNP, NaturalParametrization
+from efax import MultivariateUnitNormalNP
 from jax import enable_custom_prng, grad, jit, vjp, vmap
 from jax._src.prng import PRNGKeyArray, threefry_prng_impl
 from jax.lax import dot, stop_gradient
@@ -21,7 +19,7 @@ from jax.tree_util import tree_map
 from jaxopt import GradientDescent
 from more_itertools import mark_ends
 from tjax import RealArray, RealNumeric, custom_vjp, print_generic
-from tjax.dataclasses import dataclass, field
+from tjax.dataclasses import dataclass
 from tjax.gradient import Adam, GradientState, GradientTransformation
 
 
@@ -45,7 +43,7 @@ def cli() -> None:
         print_generic(state)
 
 
-@p_dataclass
+@dataclass
 class DeductionDataSource:
     dataset: list[tuple[Any, Any]]
 
