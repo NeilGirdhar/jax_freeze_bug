4c576a51cd2320172c5eba70ec5e4dec67d66d4a
Neil Girdhar <mistersheik@gmail.com>
2023-01-27 03:06:33 -0500

    Fix annotations

--- jfb/one.py
+++ jfb/one.py
@@ -28,11 +28,11 @@ class Weights:
 @dataclass
 class SolutionState:
     gradient_state: GradientState
-    weights: Array
+    weights: Weights
 
 
 def cli() -> None:
-    gradient_transformation = Adam[Array](1e-2)
+    gradient_transformation = Adam[Weights](1e-2)
     weights = Weights(b1=jnp.array([0., 0., 0.], dtype=np.float32),
                       w1=jnp.array([[-0.667605, 0.3261746, -0.0785462]], dtype=np.float32),
                       b2=jnp.array([0.], dtype=np.float32),
@@ -63,7 +63,7 @@ def cli() -> None:
 @jit
 def train_one_episode(observation: Array,
                       state: SolutionState,
-                      gradient_transformation: GradientTransformation[Any, Array],
+                      gradient_transformation: GradientTransformation[Any, Weights],
                       ) -> SolutionState:
     observations = jnp.reshape(observation, (1, 1))
     weights_bar, observation = _v_infer_gradient_and_value(observations, state.weights)
@@ -73,18 +73,18 @@ def train_one_episode(observation: Array,
     return SolutionState(new_gradient_state, new_weights)
 
 
-def _infer(observation: Array, weights: Array) -> tuple[Array, Array]:
+def _infer(observation: Array, weights: Weights) -> tuple[Array, Array]:
     seeker_loss = internal_infer_co(observation, weights)
     return seeker_loss, observation
 
 
-def _infer_gradient_and_value(observation: Array, weights: Array) -> tuple[Array, Array]:
+def _infer_gradient_and_value(observation: Array, weights: Weights) -> tuple[Weights, Array]:
     bound_infer = partial(_infer, observation)
     f: Callable[[Array], tuple[Array, Array]] = grad(bound_infer, has_aux=True)
     return f(weights)
 
 
-def _v_infer_gradient_and_value(observations: Array, weights: Array) -> tuple[Array, Array]:
+def _v_infer_gradient_and_value(observations: Array, weights: Weights) -> tuple[Weights, Array]:
     f = vmap(_infer_gradient_and_value, in_axes=(0, None), out_axes=(0, 0))
     weights_bars, infer_outputs = f(observations, weights)
     weights_bar = tree_map(partial(jnp.mean, axis=0), weights_bars)
@@ -95,7 +95,7 @@ def odd_power(base: Array, exponent: Array) -> Array:
     return jnp.copysign(jnp.abs(base) ** exponent, base)
 
 
-def energy(natural_explanation: Array, observation: Array, weights: Array) -> Array:
+def energy(natural_explanation: Array, observation: Array, weights: Weights) -> Array:
     p = observation
     q = (dot(softplus(dot(natural_explanation, softplus(weights.w1)) + weights.b1),
              softplus(weights.w2))
@@ -103,7 +103,7 @@ def energy(natural_explanation: Array, observation: Array, weights: Array) -> Ar
     return dot(p - q, p) + 0.5 * jnp.sum(jnp.square(q)) - 0.5 * jnp.sum(jnp.square(p))
 
 
-def internal_infer(observation: Array, weights: Array) -> Array:
+def internal_infer(observation: Array, weights: Weights) -> Array:
     minimizer = GradientDescent(energy, has_aux=False, maxiter=250, tol=0.001, acceleration=False)
     minimizer_result = minimizer.run(jnp.zeros(1), observation=observation, weights=weights)
     return jnp.sum(jnp.square(minimizer_result.params)) * 1e-1
@@ -113,11 +113,11 @@ _Weight_VJP = Callable[[Array], tuple[Array]]
 
 
 @custom_vjp
-def internal_infer_co(observation: Array, weights: Array) -> Array:
+def internal_infer_co(observation: Array, weights: Weights) -> Array:
     return internal_infer(observation, weights)
 
 
-def internal_infer_co_fwd(observation: Array, weights: Array) -> tuple[Array, _Weight_VJP]:
+def internal_infer_co_fwd(observation: Array, weights: Weights) -> tuple[Array, _Weight_VJP]:
     return vjp(partial(internal_infer, observation), weights)
 
 
